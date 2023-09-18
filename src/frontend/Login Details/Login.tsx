5427573cb2752c259365c38421726bf94a241fd9
AIFATS <info@aifats.com>
2023-09-18 21:36:02 +0530

    y

--- src/frontend/Login Details/Login.tsx
+++ src/frontend/Login Details/Login.tsx
@@ -17,7 +17,7 @@ function App() {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/auth/login', {
+      const response = await axios.post('http://127.0.0.1:8000/auth/login', {
         email,
         password
       });
