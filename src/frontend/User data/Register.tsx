7a217f2076969797c792cdb1c3367c2a40558e8a
Abhishek1Mandal <101693497+Abhishek1Mandal@users.noreply.github.com>
2023-09-24 16:53:51 +0530

    pu

--- src/frontend/User data/Register.tsx
+++ src/frontend/User data/Register.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
-import { GoogleLogin, GoogleOAuthProvider, useGoogleOneTapLogin } from '@react-oauth/google';
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import img from "../../Components/assets/imgs/christmas.jpg";
 import jwt_decode from "jwt-decode";
 
@@ -39,52 +39,22 @@ const Register: React.FC = () => {
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl ">
                   Sign up Your account
                 </h1>
-                <div className="flex flex-col sm:flex-row sm:space-x-2">
-                  <GoogleOAuthProvider clientId="536585599787-4a44c9aq46ifgsm66mfriea6uuvnuft2.apps.googleusercontent.com">
-                    {/* <Link
-                    to=""
-                    type="button"
-                    className="text-white rounded-md bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium text-sm px-3 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
-                  > */}
-                    <div className=" w-96">
-                      <GoogleLogin
-                        onSuccess={(credentialResponse) => {
-                          if (credentialResponse.credential) {
-                            var decoded = jwt_decode(credentialResponse.credential);
-                            console.log(decoded);
-                          } else {
-                            console.error('Credential is undefined');
-                          }
-                        }}
-                        onError={() => {
-                          console.log('Login Failed');
-                        }}
-                      />
-                    </div>
 
-                    {/* </Link> */}
-                  </GoogleOAuthProvider>
-                  {/* <Link
-                    to=""
-                    type="button"
-                    className="text-white rounded-md bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium text-sm px-3 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
-                  >
-                    <svg
-                      className="w-4 h-4 mr-2"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                    Sign in with Github
-                  </Link> */}
-                </div>
+                <GoogleOAuthProvider clientId="536585599787-4a44c9aq46ifgsm66mfriea6uuvnuft2.apps.googleusercontent.com">
+                  <GoogleLogin
+                    onSuccess={(credentialResponse) => {
+                      if (credentialResponse.credential) {
+                        var decoded = jwt_decode(credentialResponse.credential);
+                        console.log(decoded);
+                      } else {
+                        console.error("Credential is undefined");
+                      }
+                    }}
+                    onError={() => {
+                      console.log("Login Failed");
+                    }}
+                  />
+                </GoogleOAuthProvider>
 
                 <div className="flex items-center justify-center space-x-4">
                   <div className="w-full h-0.5 bg-black"></div>
@@ -95,9 +65,7 @@ const Register: React.FC = () => {
                 </div>
 
                 <form className="space-y-4 md:space-y-6" action="">
-
                   <div className="grid grid-cols-2 gap-2">
-
                     <div>
                       <label
                         htmlFor="firstname"
@@ -129,7 +97,6 @@ const Register: React.FC = () => {
                         placeholder=""
                       />
                     </div>
-
                   </div>
 
                   <div>
@@ -347,7 +314,6 @@ const Register: React.FC = () => {
                   >
                     Sign up
                   </Link>
-
                 </form>
               </div>
             </div>
