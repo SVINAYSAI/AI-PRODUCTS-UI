8443f098b1e40a3b262a81585b15e24919ee707f
AIFATS <info@aifats.com>
2023-09-30 20:14:58 +0530

    34

--- src/Products/Pages/3d_object/sketchfab/sketchfab_models.tsx
+++ src/Products/Pages/3d_object/sketchfab/sketchfab_models.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import SketchfabViewer from './SketchfabViewer';
 
 const SketchfabSearch: React.FC = () => {
-  const [data, setData] = useState<{ next: string; uid: string[] } | null>(null);
+  const [data, setData] = useState<{ next: string; thumbnail_images: { url: string }[] } | null>(null);
 
   useEffect(() => {
     fetch('http://localhost:5000/get_sketchfab_data')
@@ -18,13 +17,16 @@ const SketchfabSearch: React.FC = () => {
         <div>
           <p className="mb-2">Next URL: {data.next}</p>
           <ul>
-            {data.uid.map((uid, index) => (
+            {data.thumbnail_images.map((image, index) => (
               <li key={index} className="mb-1">
                 <div className="grid grid-cols-4 gap-4">
-                <a href={uid} target="_blank" rel="noopener noreferrer" className="text-blue-500">
-                <SketchfabViewer modelUid={uid} />
-                  {/* {uid} */}
-                </a>
+                  {image ? (
+                    <a href={image.url} target="_blank" rel="noopener noreferrer" className="text-blue-500">
+                      <img src={image.url} alt={`Thumbnail ${index}`} />
+                    </a>
+                  ) : (
+                    <p>No image available</p>
+                  )}
                 </div>
               </li>
             ))}
