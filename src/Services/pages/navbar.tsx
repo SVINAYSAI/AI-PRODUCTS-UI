f618b275dfe114015aeb941c7ddfd2a577479fe3
AIFATS <info@aifats.com>
2023-10-15 17:52:19 +0530

    qw

--- src/Services/pages/navbar.tsx
+++ src/Services/pages/navbar.tsx
@@ -4,9 +4,10 @@ import "../../components/assets/css/navbar_background.css"
 
 export default function Navbar() {
   return (
-    <div className="video1 container mx-auto px-4">
+    <div className="video1 container mx-auto px-4 shadow-md">
+      <h1>de</h1>
       <div className="relative">
-        <video className="absolute w-full h-full object-cover" autoPlay loop muted>
+        <video className="video1 absolute w-full h-full object-cover" autoPlay loop muted>
           <source src={videoSrc} type="video/mp4" />
         </video>
       </div>
