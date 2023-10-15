3c419952f708387751837232c05ff203773d8b0f
Abhishek1Mandal <101693497+Abhishek1Mandal@users.noreply.github.com>
2023-10-15 15:52:41 +0530

    delete

--- src/Services/pages/loadVideos.ts
+++
@@ -1,9 +0,0 @@
-// import "../../Components/assets/background video/video"
-const videoContext = (require as any).context('../../components/assets/background video/images', false, /\.(mp4|webm|ogg)$/);
-
-const loadVideoPaths = () => {
-    const paths = videoContext.keys().map(videoContext);
-    return paths;
-};
-
-export default loadVideoPaths;
