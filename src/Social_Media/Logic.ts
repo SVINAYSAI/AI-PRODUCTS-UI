4ee27da5c47ba9451436f1b1950d5a3e070f5f4e
Abhishek1Mandal <101693497+Abhishek1Mandal@users.noreply.github.com>
2024-01-07 20:39:36 +0530

    logic

--- src/Social_Media/Logic.ts
+++ src/Social_Media/Logic.ts
@@ -1,49 +1,59 @@
-import react from "react";
-import React, { useState, ChangeEvent, useEffect } from "react";
-import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
+// Logic.ts
+import { useState, ChangeEvent } from "react";
+import { EmojiClickData } from "emoji-picker-react";
 
-export const Logic =  () => { 
-    const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-    const [filePreviews, setFilePreviews] = useState<(string | ArrayBuffer | null)[]>([]);
-    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-    const [editorContent, setEditorContent] = useState<string>("");
+interface LogicProps {
+  selectedFiles: File[];
+  setSelectedFiles: React.Dispatch<React.SetStateAction<File[]>>;
+  setFilePreviews: React.Dispatch<React.SetStateAction<(string | ArrayBuffer | null)[]>>;
+  setShowEmojiPicker: React.Dispatch<React.SetStateAction<boolean>>;
+  setEditorContent: React.Dispatch<React.SetStateAction<string>>;
+}
 
-    const handleFileChangepriview = (event: ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        if (files) {
-          const newFiles = Array.from(files);
-          setSelectedFiles([...selectedFiles, ...newFiles]);
-          const newPreviews = newFiles.map((file) => {
-            const reader = new FileReader();
-            if (file.type.startsWith("image/")) {
-              reader.readAsDataURL(file);
-            } else if (file.type.startsWith("video/")) {
-              reader.readAsArrayBuffer(file);
-            } else {
-              setFilePreviews((prevPreviews) => [...prevPreviews, null]);
-              return "";
-            }
-            reader.onload = () => {
-              setFilePreviews((prevPreviews) => [...prevPreviews, reader.result]);
-            };
-            return "";
-          });
+export const useLogic = ({
+  selectedFiles,
+  setSelectedFiles,
+  setFilePreviews,
+  setShowEmojiPicker,
+  setEditorContent,
+}: LogicProps) => {
+  const handleFileChangepriview = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const newFiles = Array.from(files);
+      setSelectedFiles([...selectedFiles, ...newFiles]);
+      const newPreviews = newFiles.map((file) => {
+        const reader = new FileReader();
+        if (file.type.startsWith("image/")) {
+          reader.readAsDataURL(file);
+        } else if (file.type.startsWith("video/")) {
+          reader.readAsArrayBuffer(file);
+        } else {
+          setFilePreviews((prevPreviews) => [...prevPreviews, null]);
+          return "";
         }
-      };
-    
-      const handleEmojiClick = (emojiObject: EmojiClickData, event: MouseEvent) => {
-        setEditorContent((prevContent) => prevContent + emojiObject.emoji);
-      };
-    
-      const handleEmojiButtonClick = (
-        event: React.MouseEvent<HTMLButtonElement>
-      ) => {
-        event.stopPropagation();
-        setShowEmojiPicker(!showEmojiPicker);
-      };
+        reader.onload = () => {
+          setFilePreviews((prevPreviews) => [...prevPreviews, reader.result]);
+        };
+        return "";
+      });
+    }
+  };
 
-    return(
-        handleFileChangepriview,
-        handleEmojiClick,
-    );
+  const handleEmojiClick = (emojiObject: EmojiClickData, event: MouseEvent) => {
+    setEditorContent((prevContent) => prevContent + emojiObject.emoji);
+  };
+
+  const handleEmojiButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.stopPropagation();
+    setShowEmojiPicker((prevShowEmojiPicker) => !prevShowEmojiPicker);
+  };
+
+  return {
+    handleFileChangepriview,
+    handleEmojiClick,
+    handleEmojiButtonClick,
+  };
 };
