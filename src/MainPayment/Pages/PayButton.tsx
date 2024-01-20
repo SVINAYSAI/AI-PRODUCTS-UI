3de47e765c6b9996ef154d6a670278e90e571b45
AIFATS <info@aifats.com>
2024-01-20 12:26:46 +0530

    a

--- src/MainPayment/Pages/PayButton.tsx
+++ src/MainPayment/Pages/PayButton.tsx
@@ -9,44 +9,36 @@ interface Order {
 export default function App() {
   const [Razorpay, isLoaded] = useRazorpay();
 
-  const createOrder = async (params: any): Promise<Order> => {
-    // Implement your createOrder logic here
-    // For example, you might make an API call to create an order and return it
-    const order: Order = {
-      id: "order_NQS7xPI52kzbDg",
-    };
-    return order;
-  };
 
   const handlePayment = useCallback(async () => {
     try {
-      const params = {}; // Define your params object or fetch it as needed
-      const order = await createOrder(params);
+      
+      
 
       const options: RazorpayOptions = {
         key: "rzp_test_dIEAmku2P7C3UW",
-        amount: String(3000),
+        amount: "3000",
         currency: "INR",
         name: "Acme Corp",
-        // description: "Test Transaction",
-        // image: "https://example.com/your_logo",
-        order_id: order.id,
-        // customer_id:"",
-        // send_sms_hash: true,
-        // handler: (res) => {
-        //   console.log(res);
-        // },
-        // prefill: {
-        //   name: "Piyush Garg",
-        //   email: "youremail@example.com",
-        //   contact: "9999999999",
-        // },
-        // notes: {
-        //   address: "Razorpay Corporate Office",
-        // },
-        // theme: {
-        //   color: "#3399cc",
-        // },
+        description: "Test Transaction",
+        image: "https://example.com/your_logo",
+        order_id: "order_NQm6aqL38cRvkM",
+        customer_id:"",
+        send_sms_hash: true,
+        handler: (res) => {
+          console.log(res);
+        },
+        prefill: {
+          name: "Piyush Garg",
+          email: "youremail@example.com",
+          contact: "9999999999",
+        },
+        notes: {
+          address: "Razorpay Corporate Office",
+        },
+        theme: {
+          color: "#3399cc",
+        },
         
       };
 
@@ -56,7 +48,7 @@ export default function App() {
     } catch (error) {
       console.error("Error handling payment:", error);
     }
-  }, [Razorpay, createOrder]);
+  }, [Razorpay]);
 
   useEffect(() => {
     if (isLoaded) {
