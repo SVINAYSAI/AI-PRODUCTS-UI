caf64317f459f2832f665181483455c8c546d84e
AIFATS <info@aifats.com>
2024-01-29 15:24:47 +0530

    w

--- src/HOME/MainPages/Home.tsx
+++ src/HOME/MainPages/Home.tsx
@@ -14,6 +14,7 @@ function App() {
  
   return (
     <>
+    
       <Header />
       <main>
         <WeCreate />
@@ -25,6 +26,7 @@ function App() {
         {/* <Chatbot/> */}
       </main>
       <Footer />
+    
     </>
   );
 }
